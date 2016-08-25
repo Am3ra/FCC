c44f19b166345c4696ac4ca9e931de0aa2136321
jp2kk2 <JP2KK2@GMAIL.COM>
2016-08-24 22:55:45 -0500

    Update decided rance.js

--- Easy-Challenges/random/decided rance.js
+++ Easy-Challenges/random/decided rance.js
@@ -12,6 +12,9 @@ ourRandomRange(1, 9);
 function randomRange(myMin, myMax) {
 
   return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // What it does: Take random number, multiplies by (the range +1), as Math.random() gives decimal between 0 and 1
+  //Then, it rounds the result down (Math.floor) and it adds the min, so that the range begins in the min
+  //Result is a radom range.
 }
 
 // Change these values to test your function
