bad0818bb89991b188e51df1769bc9db874532f7
am3ra <jp2kk2@gmail.com>
2017-12-04 11:26:06 -0600

    wiki viewer js

--- Harder challenges/Wikipedia Viewer/index.js
+++ Harder challenges/Wikipedia Viewer/index.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
   var search  = $("#Search"),
       results = $("#results"),
-      input   = $("#input");
- search.on("click",function(){
-  var value = input.val(),
-    url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=";
-  results.html("<h2>Results:</h2>");
-  url += string(value);
-   $.getJSON(url, function (json) {
-     results.append(JSON.stringify(json));
-   });
-  results.show("slow");
-  
-  return false
- });
+      input   = $("#input"),
+      page = "https://www.wikiwand.com/en/";
+  search.on("click",function(){
+    results.hide();
+    var value = input.val();
+    var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=" + value + "&callback=?";
+    results.html("<h2>Results:</h2>");
 
+    $.getJSON(url, function(json){
+      results.append("<h4 class='text-Muted'> Total Hits: " + json.query.searchinfo.totalhits +"</h4>");
+      json.query.search.forEach(element => {
+        results.append("<div class='card mt-5 mb-5 bg-faded'><div class='card-block bg-faded m-5'><h2>" + element.title + "</h2><h4>" + element.snippet + " </h4>" + "<a href='"+ page + element.title + "' class='btn btn-outline-primary' role='button'> Go to page </a>" + "</div></div>")
+      });
+    });
+    
+    results.show("slow");
+    return false
+  });
 });
