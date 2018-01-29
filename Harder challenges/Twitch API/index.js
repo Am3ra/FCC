d3f1186ba1d861c4ca46868d155d9e59a36cee33
am3ra <jp2kk2@gmail.com>
2018-01-29 00:13:55 -0600

    FCC twitch stuff start

---
+++ Harder challenges/Twitch API/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //https://wind-bow.glitch.me/twitch-api/streams/
+    var channels = ["esl_csgo", "freecodecamp", "chessnetwork"]
+
+
+    $('.btn-group .btn input').on('change', function () {
+        alert("wow");
+    });
+});
