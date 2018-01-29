8283a7427d37bd9cde6b53c6db3af0fe01faf70f
am3ra <jp2kk2@gmail.com>
2018-01-29 14:43:05 -0600

    clase Joe

--- Harder challenges/Twitch API/index.js
+++ Harder challenges/Twitch API/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     //https://wind-bow.glitch.me/twitch-api/streams/
-    var channels = ["esl_csgo", "freecodecamp", "chessnetwork"]
-
-
+    var channels = ["esl_csgo", "freecodecamp", "chessnetwork","monstercat"];
+    var status = {};
+    var url = "https://wind-bow.glitch.me/twitch-api/streams/";
     $('.btn-group .btn input').on('change', function () {
-        alert("wow");
+        alert(this.id);
     });
 });
