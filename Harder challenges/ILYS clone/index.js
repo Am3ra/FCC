11bfd0afa9d4a66088c9f7de984806279ff65f51
am3ra <jp2kk2@gmail.com>
2018-04-16 19:46:51 -0500

    remove site challenges

--- Harder challenges/ILYS clone/index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var a= "", b;
-    pressKey();
-    function pressKey() {
-        a += document.getElementById('myInput').value;
-        document.getElementById('myInput').value = "";
-        document.getElementById("demo").innerHTML = ""+ a.split(" ").length;
-    }
-}
--- Harder challenges/Twitch API/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    //https://wind-bow.glitch.me/twitch-api/streams/
-    var channels = ["esl_csgo", "freecodecamp", "chessnetwork","monstercat"];
-    var status = {};
-    var url = "https://wind-bow.glitch.me/twitch-api/streams/";
-    $('.btn-group .btn input').on('change', function () {
-        alert(this.id);
-    });
-});
